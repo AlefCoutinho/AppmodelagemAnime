94670972e5876826b63c49cddeac4f0b7eedb5f0
ericdourado <ericdourado1@hotmail.com>
2022-10-01 17:49:49 -0300

    Adicionado crud na pagina anime e usuario

---
+++ src/components/AnimeForm.vue
@@ -0,0 +1,257 @@
+<template>
+    <v-container>
+        <v-row class="text-center">
+            <v-col offset-lg="2" lg="8" md="12">
+                <v-form v-model="valid">
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field label="Nome" v-model="anime.nome"></v-text-field>
+                            <v-text-field label="Diretor" v-model="anime.diretor" ></v-text-field>
+                            <v-text-field label="Estudio" v-model="anime.estudio" ></v-text-field>
+                            <v-select v-model="anime.qtdtemporadas" :items="itemsQtdTemp" label="Quantidade de temporadas"> </v-select>
+                            <v-textarea
+                                v-model="anime.sinopse" 
+                                label="Sinopse"
+                                value="Digite aqui a sinopse">
+                            </v-textarea>
+
+                            <v-select v-model="addCategoria" :items="categorias" label="Categorias"> </v-select>
+                            <v-btn text @click="setCategoria">
+                                <v-icon>mdi-plus</v-icon>
+                                <span class="mr-2">Categoria</span>
+                            </v-btn>
+                            <ul> 
+                                <li v-for="(cat, indice) in anime.categorias" :key="indice"> {{cat}}
+                                    <v-btn class="red lighten-1 white--text" @click="removeCategoria(indice) "> <v-icon dark left>mdi-cancel  </v-icon></v-btn>
+                                </li>
+                                
+                            </ul>
+
+                            <v-select v-model="addPlataforma" :items="plataformas" label="Plataformas"> </v-select>
+                            <v-btn text @click="setPlataforma">
+                                <v-icon>mdi-plus</v-icon>
+                                <span class="mr-2">Plataforma</span>
+                            </v-btn>
+                            
+                              <ul> 
+                                <li v-for="(plat, indice) in anime.plataformas" :key="indice">  {{plat}} 
+                                    <v-btn class="red lighten-1 white--text" @click="removePlataforma(indice)"> <v-icon dark left>mdi-cancel  </v-icon></v-btn>
+                                </li>
+                                
+                            </ul>
+
+                            
+                            <v-text-field type="date" label="Data de lançamento" v-model="anime.dtlancamento"  ></v-text-field>
+                            <v-text-field type="number" label="Quantidade de episódios" v-model="anime.qtdEp"  ></v-text-field>
+                            <v-select v-model="anime.statusDoAnime" :items="statusDoAnime" label="Status Do Anime"> </v-select>
+                            <v-text-field type="number" label="Nota" v-model="anime.nota" ></v-text-field> 
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" class="text-right">
+                            <v-btn class="primary" @click="salvarAnime">Salvar</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </v-col>
+        </v-row>
+
+        <v-row class="text-center lighten-4 blue">
+            <v-col offset-lg="2" lg="8" md="12">
+                <v-simple-table>
+                    <thead>
+                        <tr>
+                            <th class="text-left" style="width: 60%">Nome do anime</th>
+                            <th class="text-left">Diretor</th>
+                            <th class="text-left">Quantidade de temporadas</th>
+                            <th class="text-left">Categoria</th>
+                            <th class="text-left">Sinopse</th>
+                            <th class="text-left">Categoria</th>
+                            <th class="text-left">Plataforma</th>
+                            <th class="text-left">Data de lançamento</th>
+                            <th class="text-left">Quantidade de episódios</th>
+                            <th class="text-left">Status do Anime</th>
+                            <th class="text-left">Nota</th>
+                        
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(anime, indice) in listaAnime" :key="indice">
+                            <td class="text-left">{{ anime.nome }}</td>
+                            <td class="text-left">{{ anime.diretor }}</td>
+                            <td class="text-left">{{ anime.estudio }}</td>
+                            <td class="text-left">{{ anime.qtdtemporadas }}</td>
+                            <td class="text-left">{{ anime.sinopse }}</td>
+                            <td class="text-left">{{ anime.categorias }}</td>
+                            <td class="text-left">{{ anime.plataformas }}</td>
+                            <td class="text-left">{{ anime.dtlancamento }}</td>
+                            <td class="text-left">{{ anime.qtdEp }}</td>
+                            <td class="text-left">{{ anime.statusDoAnime }}</td>
+                            <td class="text-left">{{ anime.nota }}</td>
+                           
+                            
+                            <td>
+                                <v-btn class="orange  lighten-1 white--text" @click="alterarAnime(anime)">
+                                    <v-icon left>mdi-wrench </v-icon>
+                                    Alterar
+                                </v-btn>
+                                <v-btn class="red lighten-1 white--text" @click="removerAnime(anime.id)">
+                                    <v-icon dark left>mdi-cancel </v-icon>
+                                    Remover
+                                </v-btn>
+                            </td>
+                        </tr>
+                    </tbody>
+                </v-simple-table>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+
+<script>
+export default {
+    name: "animeForm",
+    data: () => ({
+        valid: true,
+        anime: {
+            nome: "",
+            diretor: "",
+            estudio: "",
+            qtdtemporadas: "",
+            sinopse: "",
+            categorias: [],
+            plataformas: [],
+            dtlancamento: "",
+            qtdEp: "",
+            statusDoAnime: "",
+            nota: "",
+        },
+        errors: "",
+        addCategoria: "",
+        categorias: [],
+        addPlataforma: "",
+        plataformas: [],
+        itemsQtdTemp: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+        statusDoAnime: ["Finalizado", "Em lançamento", "Em produção"],
+        listaAnime: [],
+        indice: -1,
+    }),
+    mounted() {
+        this.getAnime();
+        this.getCategorias();
+        this.getPlataformas();
+    },
+    methods: {
+
+        setCategoria() {
+            if (!this.anime.categorias.includes(this.addCategoria) && this.addCategoria != "") {
+                this.anime.categorias.push(this.addCategoria);
+                this.addCategoria = "";
+            }else{
+                this.errors = "Não deve repetir a mesma categoria";
+            }
+        },
+        removeCategoria(indice) {
+            this.anime.categorias.splice(indice,1);
+        },
+        setPlataforma() {
+            if (!this.anime.plataformas.includes(this.addPlataforma) && this.addPlataforma != "") {
+                this.anime.plataformas.push(this.addPlataforma);
+                this.addPlataforma = ""; 
+            }else{
+                this.errors = "Não deve repetir a mesma plataforma";
+            }
+        },
+        removePlataforma(indice) {
+            this.anime.plataformas.splice(indice,1);
+        },
+        async getCategorias() {
+            const req = await fetch('http://localhost:3000/categorias');
+            const data = await req.json();
+            data.map(data =>  {
+                this.categorias.push(data.nome);
+            });  
+        },
+        async getPlataformas() {
+            const req = await fetch('http://localhost:3000/plataforma');
+            const data = await req.json();
+            data.map(data =>  {
+                this.plataformas.push(data.plataforma);
+            });  
+        },
+        async getAnime() {
+            const req = await fetch('http://localhost:3000/anime');
+            const data = await req.json();
+            this.listaAnime = data;
+        },
+        limpaAnime() {
+            this.anime = {
+                id: 0,
+                anime: "",
+                email: "",
+                senha: ""
+            };
+            this.indice = -1;
+        },
+        async salvarAnime() {
+            if(this.indice < 0){
+                
+                const objData = JSON.stringify(this.anime);
+                const req = await fetch('http://localhost:3000/anime',
+                {
+                    method: "POST",
+                    body: objData,
+                    headers: { 
+                        "Content-type": "application/json"
+                    }
+                });
+                const data = await req.json();
+                console.log(data);
+
+            }else{
+
+                const objData = JSON.stringify(this.anime);
+                this.alterarAnime(this.anime);
+                const req = await fetch('http://localhost:3000/anime/'+ this.indice,
+                {   
+                    method: "PUT",
+                    body: objData,
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                });
+                const data = await req.json();
+                this.listaAnime = data;
+            }
+
+            this.getAnime();
+            this.limpaAnime();
+        },
+
+        async alterarAnime(anime){
+            this.indice = anime.id;
+            this.anime = anime;
+
+        },
+        async removerAnime(indice){
+        const req = await fetch('http://localhost:3000/anime/'+indice,
+        {
+            method: "DELETE",
+            headers: { 
+                "Content-type": "application/json"
+            }
+        });
+        const data = await req.json();
+        console.log(data);
+        this.getAnime();
+
+        }
+    }
+
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -2,56 +2,50 @@
     <v-container>
         <v-row class="text-center">
             <v-col offset-lg="2" lg="8" md="12">
-            <v-row>
-            <v-col cols="12">
-            <v-text-field label="usuario" v-model="plataforma.plataforma" ></v-text-field>
-            <v-select
-                v-model="plataforma.tipoServico"
-                :items="items"
-                label="Tipo de Serviço">
-            </v-select>
+                <v-form v-model="valid">
+                    <v-row>
+                        <v-col cols="12">
 
+                            <v-text-field label="Usuario" v-model="usuario.login" :rules="loginRules" required></v-text-field>
+                            <v-text-field v-model="usuario.email" :rules="emailRules" label="E-mail" required></v-text-field>
+                            <v-text-field type="password" label="Senha" :rules="senhaRules" v-model="usuario.senha" required></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" class="text-right">
+                            <v-btn class="primary" @click="salvarUsuario">Salvar</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </v-col>
+        </v-row>
 
-            </v-col>
-            </v-row>
-            <v-row>
-            <v-col cols="12" class="text-right">
-                <v-btn class="primary" @click="salvarPlataforma">Salvar</v-btn>
-            </v-col>
-            </v-row>
-            </v-col>
-            </v-row>
-            <v-row class="text-center lighten-4 blue">
+        <v-row class="text-center lighten-4 blue">
             <v-col offset-lg="2" lg="8" md="12">
-            <v-simple-table>
-                <thead>
-                <tr>
-                    <th class="text-left" style="width: 60%">Plataforma</th>
-                    <th class="text-left">Tipo Serviço</th>
-                    <th class="text-left">Ação</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(plataforma, indice) in listaPlataforma"
-                        :key="indice"
-                        :class="plataforma.feita ? 'lighten-4 orange' : ''"
-                    >
-                        <td class="text-left">{{ plataforma.plataforma }}</td>
-                        <td class="text-left">{{ plataforma.tipoServico }}</td>
-                        <td>
-                            <v-btn class="orange  lighten-1 white--text" @click="alterarPlataforma(plataforma)">
-                                <v-icon left>mdi-wrench </v-icon>
-                                Alterar
-                            </v-btn>
-                            <v-btn class="red lighten-1 white--text" @click="removerPlataforma(plataforma.id)">
-                                <v-icon dark left>mdi-cancel </v-icon>
-                                Remover
-                            </v-btn>
-
-                        </td>
-                    </tr>
-                </tbody>
-            </v-simple-table>
+                <v-simple-table>
+                    <thead>
+                        <tr>
+                            <th class="text-left" style="width: 60%">usuario</th>
+                            <th class="text-left">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(usuario, indice) in listaUsuario" :key="indice">
+                            <td class="text-left">{{ usuario.login }}</td>
+                            <td class="text-left">{{ usuario.email }}</td>
+                            <td>
+                                <v-btn class="orange  lighten-1 white--text" @click="alterarUsuario(usuario)">
+                                    <v-icon left>mdi-wrench </v-icon>
+                                    Alterar
+                                </v-btn>
+                                <v-btn class="red lighten-1 white--text" @click="removerUsuario(usuario.id)">
+                                    <v-icon dark left>mdi-cancel </v-icon>
+                                    Remover
+                                </v-btn>
+                            </td>
+                        </tr>
+                    </tbody>
+                </v-simple-table>
             </v-col>
         </v-row>
     </v-container>
@@ -59,96 +53,107 @@
 
 
 <script>
-    export default {
-        name: "UsuarioForm",
-        data: () => ({
-            plataforma: {
-            },
-            listaPlataforma: [],
-            indice: -1,
-            items: ['Pago', "Grátis"]
-
-        }),
-        mounted(){
-            this.getPlataforma();
+export default {
+    name: "UsuarioForm",
+    data: () => ({
+        valid: true,
+        loginRules: [
+        v => !!v || 'Login é obrigatório',
+        v => (v && v.length <= 10) || 'Login deve ser menor do que 10 caracters',
+      ],
+      emailRules: [
+        v => !!v || 'E-mail é obrigatório',
+        v => /.+@.+\..+/.test(v) || 'E-mail deve ser válido',
+      ],
+      senhaRules: [
+        v => !!v || 'Senha é obrigatória',
+        v => (v && v.length >= 8) || 'Senha Deve ser maior igual a 8 characters',
+      ],
+        usuario: {
+            login: "",
+            email: "",
+            senha: "",
         },
-        methods: {
-            async getPlataforma(){
-                const req = await fetch('http://localhost:3000/plataforma');
-                const data = await req.json();
-                this.listaPlataforma = data;
-                console.log(this.listaPlataforma)
-
-            },
-            limpaPlataforma() {
-                this.plataforma = {
-                    id: 0,
-                    plataforma: "",
-                    tipoServico: ""
-                };
-                this.indice = -1;
-            },
-            async salvarPlataforma() {
-                                
-                if(this.indice < 0){
-                    const objData = JSON.stringify(this.plataforma);
-                    const req = await fetch('http://localhost:3000/plataforma',
-                    {
-                        method: "POST",
-                        body: objData,
-                        headers: { 
-                            "Content-type": "application/json"
-                        }
-                    });
-                    const data = await req.json();
-                    console.log(data);
-
-                }else{
-                    
-                    const objData = JSON.stringify(this.plataforma);
-                    this.alterarPlataforma(this.plataforma);
-                    const req = await fetch('http://localhost:3000/plataforma/'+ this.indice,
-                    {   
-                        method: "PUT",
-                        body: objData,
-                        headers: {
-                            "Content-type": "application/json"
-                        }
-                    });
-                    const data = await req.json();
-                    this.listaPlataforma = data;
-                }
-
-                this.getPlataforma();
-                this.limpaPlataforma();
-            },
-
-            async alterarPlataforma(plat){
-
-                this.indice = plat.id;
-                this.plataforma = plat;
-
-
+        listaUsuario: [],
+        indice: -1,
+    }),
+    mounted() {
+        this.getUsuario();
+    },
+    methods: {
+        async getUsuario() {
+            const req = await fetch('http://localhost:3000/usuario');
+            const data = await req.json();
+            this.listaUsuario = data;
+        },
+        limpaUsuario() {
+            this.usuario = {
+                id: 0,
+                usuario: "",
+                email: "",
+                senha: ""
+            };
+            this.indice = -1;
+        },
+        async salvarUsuario() {
+            
+            if(this.indice < 0){
                 
-            },
-            async removerPlataforma(indice){
-                const req = await fetch('http://localhost:3000/plataforma/'+indice,
+                const objData = JSON.stringify(this.usuario);
+                const req = await fetch('http://localhost:3000/usuario',
                 {
-                    method: "DELETE",
+                    method: "POST",
+                    body: objData,
                     headers: { 
                         "Content-type": "application/json"
                     }
                 });
                 const data = await req.json();
                 console.log(data);
-                this.getPlataforma();
 
+            }else{
+
+                const objData = JSON.stringify(this.usuario);
+                this.alterarUsuario(this.usuario);
+                const req = await fetch('http://localhost:3000/usuario/'+ this.indice,
+                {   
+                    method: "PUT",
+                    body: objData,
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                });
+                const data = await req.json();
+                this.listaUsuario = data;
             }
-        }
 
-    };
+            this.getUsuario();
+            this.limpaUsuario();
+        },
+
+        async alterarUsuario(user){
+            this.indice = user.id;
+            this.usuario = user;
+
+        },
+        async removerUsuario(indice){
+        const req = await fetch('http://localhost:3000/usuario/'+indice,
+        {
+            method: "DELETE",
+            headers: { 
+                "Content-type": "application/json"
+            }
+        });
+        const data = await req.json();
+        console.log(data);
+        this.getUsuario();
+
+        }
+    }
+
+};
 </script>
 
 <style scoped>
-    
+
 </style>
