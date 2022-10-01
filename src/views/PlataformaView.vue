203e6c32ed18fcfc49e3bdbe793da97425cdd6d5
unknown <alef_coutin@hotmail.com>
2022-10-01 13:59:15 -0300

    alimentando a objeto  plataformas criando a página de e formulário de plataformas

--- src/views/PlataformaView.vue
+++ src/views/PlataformaView.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="plataforma">
     <h1>Plataforma Page</h1>
+    <PlataformaForm/>
   </div>
 </template>
 
-<script></script>
+<script>
+  import PlataformaForm from "../components/PlataformaForm.vue"
+
+  export default {
+    name: "PlataformaView",
+    components:{
+      PlataformaForm,
+    }
+  }
+</script>
