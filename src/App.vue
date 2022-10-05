b0b952ef09ea11c8ef9e0dce0dae483ba5bd19d3
unknown <alef_coutin@hotmail.com>
2022-10-05 13:58:55 -0300

    estilizando a barra de rolagem

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- cria a tela de aplicação vuetify -->
-  <v-app>
+  <v-app >
   <!-- cria a barra de aplicação vuetify -->
     <v-app-bar
       app
@@ -33,3 +33,25 @@
     }),
   };
 </script>
+<style>
+  /* BARRA DE ROLAGEM */
+  body::-webkit-scrollbar {
+    width: 8px; /* width of the entire scrollbar */
+  }
+
+  body::-webkit-scrollbar-track {
+    background: rgb(255, 255, 255);        /* color of the tracking area */
+  }
+
+  body::-webkit-scrollbar-thumb{
+    background-color: #cf9e25;    /* color of the scroll thumb */
+    border-radius: 0 20px 0 20px;       /* roundness of the scroll thumb */
+    /*border: 3px solid #ffeb3b;  /*creates padding around scroll thumb*/
+    border: 1px solid #fde402;  /*creates padding around scroll thumb*/
+  }
+  body::-webkit-scrollbar-thumb:hover {
+    background-color: #fde402;    /* color of the scroll thumb */
+    border-radius: 0 20px 0 20px;       /* roundness of the scroll thumb */
+    /* border: 3px solid orange;  creates padding around scroll thumb */
+  }
+</style>
